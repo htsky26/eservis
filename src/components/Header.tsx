00444ab53c418ef163423fd3acc9510e406f7ced
htsky26 <98matteo19@gmail.com>
2025-12-29 18:02:49 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-dark-bg/80 backdrop-blur-lg border-b border-dark-bg-tertiary">
+    <header className="fixed top-0 left-0 right-0 z-50 >
       <nav className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <img src={Logo}  alt="e' SERVIS" className="w-26 h-14" />
